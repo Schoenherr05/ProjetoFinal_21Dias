637e3ac1e5dbc8872558e65baecfa25374b96eff
Thiago Schoenherr <thiagoschoenherr36@gmail.com>
2024-10-01 14:42:21 -0300

    Enviando Proejto Final 21 dias

--- todoList/js/scripts.js
+++ todoList/js/scripts.js
@@ -1,39 +1,90 @@
 // Clase
 
 class ToDo {
-
+  Texto
+  Prioridade
+  Feito = false
+  constructor(texto, prioridade){
+      this.Texto = texto
+      this.Prioridade = prioridade
+  }
 }
 
 // Array
 
+let arrayToDo = []
 
 //funções projeto
 
-function CriarToDo() {
-
+function CriarToDo(texto, prioridade, array){
+  let objetoToDo = new ToDo(texto, prioridade)
+  if (!array.some(x => x.Texto == texto)){
+      array.push(objetoToDo)
+      return objetoToDo
+  }
 }
 
-function AtualizarToDo() {
-
-}
-
-function ConcluirToDo() {
-
-}
-
-function ExcluirToDo() {
-
-}
-
-function PesquisarToDo() {
- 
-}
-
-function OrdenarCrescente() {
+function AtualizarToDo(textoAntigo, textoNovo, array){
+  let atualizado = false
+  array.forEach(todo => {
+      if (todo.Texto == textoAntigo){
+          todo.Texto == textoNovo
+          atualizado = true
+      }
+  })
   
+  return atualizado
 }
-function OrdenarDecrescente() {
-  
+
+function ConcluirToDo(array, texto){
+  let concluido = false
+  array.forEach(todo => {
+      if (todo.Texto == texto){
+          if (todo.Feito){
+              todo.Feito = false
+          } else {
+              todo.Feito = true
+          }
+          concluido = true
+      }
+  })
+
+  return concluido
+}
+
+function ExcluirToDo(array, texto){
+  let index
+  let removido = false
+  array.forEach(todo => {
+      if (todo.Texto == texto){
+          index = array.indexOf(todo)
+          removido = true
+      }
+  })
+
+  array.splice(index, 1)
+  return removido
+}
+
+function PesquisarToDo(array, texto){
+  let pesquisa = false
+  array.forEach(todo => {
+      if (todo.Texto.includes(texto)){
+          pesquisa = true
+      }
+  })
+
+  return pesquisa
+}
+
+function OrdernarCrescente(array){
+  array.sort((a, b) => a.Prioridade - b.Prioridade)
+  return array
+}
+
+function OrdenarDecrescente(array){
+  array.sort((a, b) => b.Prioridade - a.Prioridade)
+  return array
 }
 
 // Seleção de elementos
